8520936693e7d9373d349fbc107e1b5812d38498
David Bentley <david.bentley95@gmail.com>
2026-02-21 17:44:10 -0800

    Minor updates to placeShip method

--- src/gameboard.js
+++ src/gameboard.js
@@ -6,7 +6,7 @@ class Gameboard {
   }
 
   _isValidCoordinate(coord) {
-    if (coor[0] < 1 || coord[0] > 10 || coord[1] < 1 || coord[1] > 10) {
+    if (coord[0] < 1 || coord[0] > 10 || coord[1] < 1 || coord[1] > 10) {
       throw new Error("Invalid coordinate, must be between 1 and 10");
     }
   }
@@ -15,15 +15,27 @@ class Gameboard {
     this._isValidCoordinate(coord);
 
     if (this.orientation === 0) {
-      for (let i = 1; i <= ship.length; i++) {
-        coord[0]++;
-        this.map.set(coord, ship);
+      for (let i = 0; i < ship.length; i++) {
+        if (i === 0) {
+          this.map.set(coord.join(","), ship);
+        } else {
+          coord[0]++;
+          this.map.set(coord.join(","), ship);
+        }
       }
-    } else if(this.orientation === 1) {
-        for (let i = 1; i <= ship.length; i++) {
+    } else if (this.orientation === 1) {
+      for (let i = 0; i < ship.length; i++) {
+        if(i === 0) {
+            this.map.set(coord.join(","), ship);
+        } else {
             coord[1]++;
-            this.map.set(coord, ship);
+            this.map.set(coord.join(","), ship);
+        }
+        
       }
     }
+    return this.map;
   }
 }
+
+module.exports = { Gameboard };
