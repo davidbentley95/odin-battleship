9e9539692183a24bbf085c4e6d2c11962dc2f477
David Bentley <david.bentley95@gmail.com>
2026-02-24 07:15:23 -0800

    Add fleet to player class

--- src/models/player.js
+++ src/models/player.js
@@ -1,11 +1,21 @@
 const { Gameboard } = require("./gameboard");
 
 class Player {
-    constructor() {
-        this.gameboard = new Gameboard();
-        this.score = 0;
-    }
-};
+  constructor() {
+    this.gameboard = new Gameboard();
+    this.score = 0;
+    this.fleet = [
+      new Ship(5),
+      new Ship(4),
+      new Ship(3),
+      new Ship(3),
+      new Ship(2),
+    ];
+  }
 
-module.exports = { Player }
+  placeFleetRandomly() {
+    
+  }
+}
 
+module.exports = { Player };
