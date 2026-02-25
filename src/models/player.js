950f553efa06289729d791ac336393a859acd730
David Bentley <david.bentley95@gmail.com>
2026-02-25 06:48:12 -0800

    Add a presetShipPlacement function

--- src/models/player.js
+++ src/models/player.js
@@ -13,8 +13,12 @@ class Player {
     ];
   }
 
-  placeFleetRandomly() {
-    
+  presetShipPlacement() {
+    this.gameboard.placeShip(fleet[0],"1,1", "vertical");
+    this.gameboard.placeShip(fleet[1],"4,5", "horizontal");
+    this.gameboard.placeShip(fleet[2],"10,3", "vertical");
+    this.gameboard.placeShip(fleet[3],"3,3", "horizontal");
+    this.gameboard.placeShip(fleet[4],"8,4", "vertical");a
   }
 }
 
