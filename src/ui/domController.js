37775a6bda85bb6208545e0fd284033676588457
David Bentley <david.bentley95@gmail.com>
2026-02-26 07:19:30 -0800

    Updated board size generation with indexes

--- src/ui/domController.js
+++ src/ui/domController.js
@@ -2,20 +2,27 @@ import { player1, player2 } from "../controllers/gameController.js";
 
 
 function createBoardGrid(player) {
-    const boardSize = player.gameboard.size * player.gameboard.size;
+    const boardSize = player.gameboard.size;
     const parentDiv = document.createElement("div");
     parentDiv.id = player.playerID;
 
-    const size = player.gameboard.size;
-    parentDiv.style.setProperty("--n", size);
+    parentDiv.style.setProperty("--n", boardSize);
 
     const main = document.querySelector("main");
     main.appendChild(parentDiv);
 
+    let coordinates = [0,0]
+
     for(let i = 1; i <= boardSize; i++) {
-        const div = document.createElement("div");
-        div.classList.add("board-cell");
-        parentDiv.appendChild(div);
+        for(let j = 1; j <= boardSize; j++) {
+            coordinates[0] = i;
+            coordinates[1] = j;
+            const div = document.createElement("div");
+            div.classList.add("board-cell");
+            div.textContent = coordinates.join(",");
+            parentDiv.appendChild(div);
+        }
+        
     }
 }
 
